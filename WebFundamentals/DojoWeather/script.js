f0c327268167fd776800d37b80061b168f519cb9
Evan <evanmschultz@gmail.com>
2023-06-14 21:55:49 -0600

    adds alert, cookie alert removal, and temp scale conversion

--- WebFundamentals/DojoWeather/script.js
+++ WebFundamentals/DojoWeather/script.js
@@ -1,5 +1,23 @@
-function myFunc() {
-    console.log("I've been called")
+function removeAlert(alertId) {
+    alert = document.getElementById(alertId)
+    alert.remove()
+}
 
-    return "Hello World"
+function updateTempScale() {
+    let temps = document.querySelectorAll("[data-type='temp'")
+    let selectElement = document.getElementById("temp-scale")
+    let selectedScale = selectElement.value
+
+    for (i = 0; i < temps.length; i++) {
+        let temp = parseInt(temps[i].innerText)
+
+        if (selectedScale === "farenheit") {
+            temp = Math.round((9 / 5) * temp + 32)
+        } else {
+            temp = Math.round((5 / 9) * (temp - 32))
+        }
+
+        elementToUpdate = document.getElementById(`temp-${i + 1}`)
+        elementToUpdate.innerText = `${temp}°`
+    }
 }
