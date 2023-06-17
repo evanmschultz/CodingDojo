b38c63e16d500e36049a0834e838c99b9c793724
Evan <evanmschultz@gmail.com>
2023-06-16 22:34:39 -0600

    adds betterThanAverage challenge

--- WebFundamentals/JavaScript/arrayChallenges.js
+++ WebFundamentals/JavaScript/arrayChallenges.js
@@ -3,6 +3,7 @@
     "food" it should console log "yummy". If "food" was not present in the 
     array console log "I'm hungry" once.
 */
+console.log("challeng 1\n")
 function alwaysHungry(array) {
     let hasFood = false
 
@@ -23,3 +24,51 @@ alwaysHungry([3.14, "food", "pie", true, "food"])
 // this should console log "yummy", "yummy"
 alwaysHungry([4, 1, 5, 7, 2])
 // this should console log "I'm hungry"
+
+/* 
+    2) Given an array and a value cutoff, return a new array containing only 
+    the values larger than cutoff. 
+*/
+console.log("\nchalleng 2\n")
+function highPassFilter(array, minimum) {
+    let tempArray = []
+
+    for (num of array) {
+        if (num > minimum) {
+            tempArray.push(num)
+        }
+    }
+
+    return tempArray
+}
+
+let hungryResult = highPassFilter([6, 8, 3, 10, -2, 5, 9], 5)
+console.log(
+    `the elements that were higher than the minimum are [${hungryResult}]`
+) // expect back [6, 8, 10, 9]
+
+/*  
+    3) Given an array of numbers return a count of how many of the numbers are larger than the average.
+*/
+console.log("\nchalleng 3\n")
+function betterThanAverage(array) {
+    let sum = 0
+
+    for (num of array) {
+        sum += num
+    }
+
+    let average = sum / array.length
+    let aboveAverage = 0
+
+    for (num of array) {
+        if (num > average) {
+            aboveAverage += 1
+        }
+    }
+
+    return aboveAverage
+}
+
+let averageResult = betterThanAverage([6, 8, 3, 10, -2, 5, 9])
+console.log(`there are ${averageResult} numbers above the average`) // expect back 4
