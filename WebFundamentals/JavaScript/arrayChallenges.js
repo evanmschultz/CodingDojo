7ccf151598869f68969b29bfedbae8757c8da2fd
Evan <evanmschultz@gmail.com>
2023-06-17 00:07:41 -0600

    adds fibonacciArray

--- WebFundamentals/JavaScript/arrayChallenges.js
+++ WebFundamentals/JavaScript/arrayChallenges.js
@@ -77,11 +77,11 @@ console.log(`there are ${averageResult} numbers above the average`) // expect ba
 console.log("\nchallenge 4\n")
 function reverseArray(array) {
     // use pointers: python a, b = b, a // JS doesn't have that without using an array, use temp variable
-    left = 0
-    right = array.length - 1
+    let left = 0
+    let right = array.length - 1
 
     while (left < right) {
-        tempValue = array[left]
+        let tempValue = array[left]
 
         array[left] = array[right]
         array[right] = tempValue
@@ -95,3 +95,33 @@ function reverseArray(array) {
 
 let reverseResult = reverseArray(["a", "b", "c", "d", "e"])
 console.log(`the reversed array is [${reverseResult}]`) // expect back ["e", "d", "c", "b", "a"]
+
+/* 5) Fibonacci numbers have been studied for years and appear often in nature. Write a function 
+that will return an array of Fibonacci numbers up to a given length n. Fibonacci numbers are calculated 
+by adding the last two values in the sequence together. So if the 4th value is 2 and the 5th value is 3 
+then the next value in the sequence is 5. */
+console.log("\nchallenge 5\n")
+function fibonacciArray(length) {
+    let outputArray = [0, 1]
+    let num = 0
+
+    if (length < 1) {
+        return []
+    }
+    if (length == 1) {
+        return outputArray
+    }
+
+    for (i = 1; i < length - 1; i++) {
+        num = outputArray[i] + outputArray[i - 1]
+
+        outputArray.push(num)
+    }
+
+    return outputArray
+}
+let inputNum = 10
+let fibonacciResult = fibonacciArray(inputNum)
+console.log(
+    `the fibonacci sequence with ${inputNum} equals [${fibonacciResult}]`
+)
