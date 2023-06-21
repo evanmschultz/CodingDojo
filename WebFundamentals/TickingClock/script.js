4854b4764cc6cea3f018feed99b12c54b51813c4
Evan <evanmschultz@gmail.com>
2023-06-21 16:56:27 -0600

    completes assignment

--- WebFundamentals/TickingClock/script.js
+++ WebFundamentals/TickingClock/script.js
@@ -1,13 +1,23 @@
-function getSecondsSinceStartOfDay() {
-    return (
-        new Date().getSeconds() +
-        new Date().getMinutes() * 60 +
-        new Date().getHours() * 3600
-    )
+const secondsHand = document.getElementById("seconds-hand")
+const minuteHand = document.getElementById("minute-hand")
+const hourHand = document.getElementById("hour-hand")
+
+function updateHandAngles() {
+    const now = new Date()
+
+    const seconds = now.getSeconds()
+    const secondsHandAngle = (seconds % 60) * 6 + 90 // 360 / 60 = 6, 1 second = 6° // add 90° to compensate for initial transform
+    secondsHand.style.transform = `rotate(${secondsHandAngle}deg)`
+
+    const minute = now.getMinutes()
+    const minuteHandAngle = (minute % 60) * 6 + 90
+    minuteHand.style.transform = `rotate(${minuteHandAngle}deg)`
+
+    const hours = now.getHours()
+    const hourHandAngle = (hours % 12) * 30 + 90 // 360 / 12 = 30, 1 hour = 30°
+    hourHand.style.transform = `rotate(${hourHandAngle}deg)`
 }
 
-setInterval(function () {
-    var time = getSecondsSinceStartOfDay()
-    console.log(time)
-    // your code here
+setInterval(() => {
+    updateHandAngles()
 }, 1000)
