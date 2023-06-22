787f8373b30ab68f311f6c1cc678319c6ee09a43
Evan <evanmschultz@gmail.com>
2023-06-22 14:47:54 -0600

    allows pacman movement

--- WebFundamentals/Games/PAC-MAN/script.js
+++ WebFundamentals/Games/PAC-MAN/script.js
@@ -2,7 +2,7 @@ let world = [
     [2, 2, 2, 2, 2, 2, 2, 2, 2, 2],
     [2, 1, 0, 2, 1, 2, 0, 1, 0, 0],
     [2, 2, 0, 2, 0, 0, 0, 2, 2, 2],
-    [2, 2, 0, 2, 2, 2, 0, 2, 1, 2],
+    [2, 2, 0, 0, 2, 2, 0, 2, 1, 2],
     [0, 0, 0, 0, 0, 0, 0, 0, 0, 2],
     [2, 2, 2, 2, 2, 1, 2, 0, 2, 2],
     [2, 1, 0, 0, 0, 0, 2, 0, 1, 2],
@@ -32,3 +32,41 @@ function displayWorld() {
 }
 
 displayWorld()
+
+let pacManPosition = {
+    x: 3,
+    y: 3,
+}
+
+function displayPacMan(x, y) {
+    // named with let to make it clear it is being updated
+    let pacMan = document.getElementById("pac-man")
+
+    // update current position in position object
+    pacManPosition.x = x
+    pacManPosition.y = y
+
+    // update the styling
+    pacMan.style.top = `${pacManPosition.y}px`
+    pacMan.style.left = `${pacManPosition.x}px`
+}
+
+document.onkeydown = function (event) {
+    const key = event.key
+
+    // get the integer value of the current position
+    let pacManTop = pacManPosition.y
+    let pacManLeft = pacManPosition.x
+
+    if (key == "ArrowRight") {
+        pacManLeft += 26 // move right one block
+    } else if (key == "ArrowLeft") {
+        pacManLeft -= 26 // move left one block
+    } else if (key == "ArrowUp") {
+        pacManTop -= 26 // subtract because it is distance from the top, move up
+    } else if (key == "ArrowDown") {
+        pacManTop += 26 // add because it is distance from the top, move down
+    }
+
+    displayPacMan(pacManLeft, pacManTop) // update display
+}
