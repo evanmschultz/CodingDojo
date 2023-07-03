b1be8d1a64026b1d75e360b0c16dfba64fa4f3e4
Evan <evanmschultz@gmail.com>
2023-07-03 14:18:07 -0600

    updates using chaining methods

--- Python/fundamentals/oop/user.py
+++ Python/fundamentals/oop/user.py
@@ -13,29 +13,40 @@ class User:
             print(f'{" ".join(key.split("_")).title()}: {value}')
 
         print('\n')
+        return self
 
     # enroll(self) - Have this method change the user's member status to True and set their gold card points to 200.
     def enroll(self):
         if self.is_rewards_member:
             print(
-                f'{self.first_name.capitalize()} {self.last_name.capitalize()} is already a member!\n')
-            return
+                f'{self.first_name.capitalize()} {self.last_name.capitalize()} is already a member!')
+            return self
 
         self.is_rewards_member = True
+        return self
 
     # spend_points(self, amount) - have this method decrease the user's points by the amount specified.
     def spend_points(self, amount):
         if amount > self.gold_card_points:
-            print(f'{self.first_name.capitalize()} doesn\'t have enough points!\n')
+            print(
+                f'{self.first_name.capitalize()} {self.last_name.capitalize()} doesn\'t have enough points!\n')
         else:
             self.gold_card_points -= amount
 
+        return self
+
 
 user = User('evan', 'schultz', 'evan@evan.com', '36', False, 500)
-user.display_info()
-user.enroll()
-user.display_info()
-user.spend_points(100)
-user.spend_points(450)
-user.display_info()
-user.enroll()
+# user.display_info()
+# user.enroll()
+# user.display_info()
+# user.spend_points(100)
+# user.spend_points(450)
+# user.display_info()
+# user.enroll()
+
+'''
+    Chaining Methods assignment
+'''
+user.display_info().enroll().spend_points(
+    100).spend_points(450).enroll().display_info()
