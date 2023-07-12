1faf7adef0abc7dfda6a6faf452ba80297a4ad59
Evan <evanmschultz@gmail.com>
2023-07-12 10:03:29 -0600

    adds list and dictionary rendering

--- Python/flask/optional_assignments/hello_flask/server.py
+++ Python/flask/optional_assignments/hello_flask/server.py
@@ -1,4 +1,5 @@
-from flask import Flask  # Import Flask to allow us to create our app
+# Import Flask to allow us to create our app
+from flask import Flask, render_template
 # Create a new instance of the Flask class called "app"
 app = Flask(__name__)
 
@@ -18,6 +19,19 @@ def success():
 def hello(name, num):
     # Return name passed in as variable after hello in the browser
     return f'Hello {name * num}'
+
+
+@app.route('/lists')
+def render_lists():
+    # Dictionary to be rendered in a table using Jinja
+    student_info = [
+        {'name': 'Michael', 'age': 35},
+        {'name': 'John', 'age': 30},
+        {'name': 'Mark', 'age': 25},
+        {'name': 'KB', 'age': 27}
+    ]
+    # Pass in a list and the dictionary to the template
+    return render_template("lists.html", random_numbers=[3, 1, 5], students=student_info)
 
 
 if __name__ == "__main__":   # Ensure this file is being run directly and not from a different module
